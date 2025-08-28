fe995bd871b411abc9195472b8c5121774a7c259
Mijail Pulgar <mijail86@gmail.com>
2025-08-28 10:20:17 -0500

    use callback

--- src/06-memos/MemoHooks.tsx
+++ src/06-memos/MemoHooks.tsx
@@ -1,17 +1,24 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import MyTitle from "./ui/MyTitle";
 import MySubtitle from "./ui/MySubtitle";
 
 const MemoHooks = () => {
   const [title, setTitle] = useState("Hola");
   const [subtitle, setSubTitle] = useState("Mundo");
+
+  // useCallback se usa cuando quiero memorizar un funcion que define en un
+  // componente que se esta renderizando pero el componente que usa la funcion no lo hace
+  const handleMyAPICall = useCallback(() => {
+    console.log("Llamar a mi API ", subtitle);
+  }, [subtitle]);
+
   return (
     <div className="bg-gradient flex flex-col gap-4">
-      <h1 className="text-2xl font-thin text-white">MomApp</h1>
+      <h1 className="text-2xl font-thin text-white">MemoApp</h1>
 
       <MyTitle title={title} />
 
-      <MySubtitle subtitle={subtitle} />
+      <MySubtitle subtitle={subtitle} callMyAPI={handleMyAPICall} />
 
       <button
         onClick={() => setTitle("Hello, " + new Date().getTime())}
--- src/06-memos/ui/MySubtitle.tsx
+++ src/06-memos/ui/MySubtitle.tsx
@@ -2,15 +2,20 @@ import React from "react";
 
 interface Props {
   subtitle: string;
+
+  callMyAPI: () => void;
 }
 
-const MySubtitle = React.memo(({ subtitle }: Props) => {
+const MySubtitle = React.memo(({ subtitle, callMyAPI }: Props) => {
   console.log("MySubTitle re-render");
   return (
     <>
       <h6 className="text-2xl font-bold">{subtitle}</h6>
 
-      <button className="bg-indigo-500 text-white px-2 py-1 rounded-md cursor-pointer">
+      <button
+        onClick={callMyAPI}
+        className="bg-indigo-500 text-white px-2 py-1 rounded-md cursor-pointer"
+      >
         Llamar a funcion
       </button>
     </>
