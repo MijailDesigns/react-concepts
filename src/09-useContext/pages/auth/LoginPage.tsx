3b8f16896f26d084d1d9fd78f020f8cfc4154aa8
Mijail Pulgar <mijail86@gmail.com>
2025-08-28 17:25:25 -0500

    navgar entre paginas - diseño de paginas

--- src/09-useContext/pages/auth/LoginPage.tsx
+++ src/09-useContext/pages/auth/LoginPage.tsx
@@ -1,5 +1,26 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Link } from "react-router";
+
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className="flex flex-col items-center min-h-screen">
+      <h1 className="text-4xl font-bold">Iniciar Sesion</h1>
+
+      <hr />
+
+      <form className="flex flex-col gap-2 my-10">
+        <Input type="number" placeholder="ID del usuario" />
+        <Button type="submit">Login</Button>
+      </form>
+
+      <Link to="/about">
+        <Button variant="ghost" className="">
+          Volver a la pagina principal
+        </Button>
+      </Link>
+    </div>
+  );
 };
 
 export default LoginPage;
--- src/09-useContext/pages/profile/ProfilePage.tsx
+++ src/09-useContext/pages/profile/ProfilePage.tsx
@@ -1,5 +1,16 @@
+import { Button } from "@/components/ui/button";
+
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-4xl">Perfil del usuario</h1>
+      <hr />
+
+      <pre className="my-4">{JSON.stringify({}, null, 2)}</pre>
+
+      <Button variant="destructive">Salir</Button>
+    </div>
+  );
 };
 
 export default ProfilePage;
