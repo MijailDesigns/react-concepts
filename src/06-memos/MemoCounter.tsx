88c4096cc8650a740d570add67d5d66e2cb10fbd
Mijail Pulgar <mijail86@gmail.com>
2025-08-28 10:50:55 -0500

    use memo react

---
+++ src/06-memos/MemoCounter.tsx
@@ -0,0 +1,45 @@
+import useCounter from "@/hooks/useCounter";
+import { useMemo } from "react";
+
+const heavyStuff = (iterationNumber: number) => {
+  console.time("Heavy stuff started");
+
+  for (let index = 0; index < iterationNumber; index++) {
+    console.log("ahi vamos...");
+  }
+
+  console.timeEnd("Heavy stuff started");
+  return `${iterationNumber} iteraciones realizadas`;
+};
+
+const MemoCounter = () => {
+  const { count, increment } = useCounter(400);
+  const { count: count2, increment: increment2 } = useCounter(10);
+  const myHeavyValue = useMemo(() => heavyStuff(count), [count]);
+  return (
+    <div className="bg-gradient flex flex-col gap-4">
+      <h1 className="text-2xl font-bold">Memo - useMemo {myHeavyValue}</h1>
+
+      <hr />
+
+      <h4>Counter: {count}</h4>
+      <h4>Counter: {count2}</h4>
+
+      <button
+        className="bg-blue-500 text-white px-4 rounded-md py-2 cursor-pointer"
+        onClick={increment}
+      >
+        +1
+      </button>
+
+      <button
+        className="bg-blue-500 text-white px-4 rounded-md py-2 cursor-pointer"
+        onClick={increment2}
+      >
+        +1 - counter2
+      </button>
+    </div>
+  );
+};
+
+export default MemoCounter;
